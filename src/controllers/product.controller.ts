87f5bf6a2a03f4948c27b0ead247496e9a9fbd9d
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-05-13 20:51:58 -0300

    feat: implementar a função create na classe ProductController

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,5 +1,5 @@
 import { StatusCodes } from 'http-status-codes';
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import ProductService from '../services/product.service';
 
 class ProductController {
@@ -8,6 +8,19 @@ class ProductController {
   public gatAll = async (req: Request, res: Response): Promise<Response> => {
     const products = await this.service.getAll();
     return res.status(StatusCodes.OK).json(products);
+  };
+
+  public create = async (req: Request, res: Response, next: NextFunction):
+  Promise<Response | void> => {
+    try {
+      const { name, amount } = req.body;
+  
+      const newProduct = await this.service.create(name, amount);
+    
+      return res.status(StatusCodes.CREATED).json(newProduct);
+    } catch (error) {
+      next(error);
+    }
   };
 }
 
