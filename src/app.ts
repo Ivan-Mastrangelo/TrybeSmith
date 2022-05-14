c31388f099d266613d5bddfe5d4b2d3626e3598b
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-05-14 00:07:50 -0300

    feat: importar e inserir middleware de erros ao app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import productRoutes from './routes/products.routes';
+import errorHandler from './middlewares/error.handler';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/products', productRoutes);
+
+app.use(errorHandler);
 
 export default app;
