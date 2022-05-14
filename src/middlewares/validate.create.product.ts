0b71698c9150cf276772deeb63c932d028aca90d
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-05-14 00:04:29 -0300

    fix: corrigir middleware de validação com o Joi das requisições de inserção de novos produtos

--- src/middlewares/validate.create.product.ts
+++ src/middlewares/validate.create.product.ts
@@ -1,21 +1,25 @@
-// import Joi from 'joi';
-// import { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
+import { Request, Response, NextFunction } from 'express';
 
-// const productSchema = Joi.object({
-//   name: Joi.string().min(3).required(),
-//   amount: Joi.string().min(3).required(),
-// });
+const productSchema = Joi.object({
+  name: Joi.string().min(3).required(),
+  amount: Joi.string().min(3).required(),
+});
 
-// const createProductMiddleware = (req: Request, res: Response, next: NextFunction) => {
-//   try {
-//     const { name, amount } = req.body;
-//     const { error } = productSchema.validate({ name, amount });
-//     if (error) throw badRequest(error.message);
-  
-//     next();
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+const validateCreateProduct = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { name, amount } = req.body;
+    const { error } = productSchema.validate({ name, amount });
+    console.log(error);
+    if (error?.details[0].type === 'any.required') {
+      return res.status(400).json({ message: error.message });
+    }
+    if (error) return res.status(422).json({ message: error.message });
 
-// export default createProductMiddleware;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default validateCreateProduct;
