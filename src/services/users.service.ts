f87c38d2a29a89020c122064dacb6a2c8e43b1e3
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-05-15 16:26:46 -0300

    feat: criar arquivo com a classe UserService com a função create

---
+++ src/services/users.service.ts
@@ -0,0 +1,16 @@
+import UserModel from '../models/users.model';
+import tokenGenerate from '../helpers/tokenGenerate';
+
+class UserService {
+  public model = new UserModel();
+
+  public create = async (username: string, classe: string, level: number, password: string):
+  Promise<string> => {
+    const { id } = await this.model.create(username, classe, level, password);
+    const userToken = tokenGenerate(id);            
+  
+    return userToken;
+  };
+}
+
+export default UserService;
