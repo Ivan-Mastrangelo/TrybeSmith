75c13fa45f42527c253b389e7c3f3dc96c79573a
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-05-15 16:35:37 -0300

    feat: inserir a rota /users ao app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import productRoutes from './routes/products.routes';
+import userRoutes from './routes/users.routes';
 import errorHandler from './middlewares/error.handler';
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/products', productRoutes);
+app.use('/users', userRoutes);
 
 app.use(errorHandler);
 
