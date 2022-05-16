8a8b14ef69a54df6dc01f1fe0156c27591acf9d4
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-05-16 14:52:24 -0300

    feat: criar o arquivo orders.service com sua classe e a função de busca dos pedidos

---
+++ src/services/orders.service.ts
@@ -0,0 +1,13 @@
+// import OrderModel from '../models/orders.model';
+// import IOrder from '../interfaces/order.interface';
+
+// class OrderService {
+//   public model = new OrderModel();
+
+//   public getAll = async (): Promise<IOrder[]> => {
+//     const orders = await this.model.getAll();
+//     return orders;
+//   };
+// }
+
+// export default OrderService;
