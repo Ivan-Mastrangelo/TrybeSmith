3319754df8cd0a45b7cb4dd4ab452407537400d6
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-05-16 14:54:51 -0300

    feat: criar a rota /orders

---
+++ src/routes/orders.routes.ts
@@ -0,0 +1,10 @@
+// import { Router } from 'express';
+// import OrderController from '../controllers/orders.controller';
+
+// const orderController = new OrderController();
+
+// const route = Router();
+
+// route.get('/', orderController.getAll);
+
+// export default route;
