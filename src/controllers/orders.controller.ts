d7502d784ff1cdc5f3d6a33a79ff3a6cddf2bc74
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-05-16 14:53:37 -0300

    feat: criar o arquivo orders.controller com sua classe e a função recebe e entrage a requisição de buscar por todos os pedidos realizados

---
+++ src/controllers/orders.controller.ts
@@ -0,0 +1,14 @@
+// import { StatusCodes } from 'http-status-codes';
+// import { Request, Response } from 'express';
+// import OrderService from '../services/orders.service';
+
+// class OrderController {
+//   public service = new OrderService();
+
+//   public getAll = async (req: Request, res: Response): Promise<Response> => {
+//     const orders = await this.service.getAll();
+//     return res.status(StatusCodes.OK).json(orders);
+//   };
+// }
+
+// export default OrderController;
