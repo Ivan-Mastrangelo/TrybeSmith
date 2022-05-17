2639dfb1aecdde2acdb80c979b9e404b59a30b56
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-05-17 20:36:34 -0300

    feat: implementar função, na classe OrderController, que recebe requisição para criação de nova order

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,5 +1,5 @@
 import { StatusCodes } from 'http-status-codes';
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import OrderService from '../services/orders.service';
 
 class OrderController {
@@ -8,6 +8,20 @@ class OrderController {
   public getAll = async (_req: Request, res: Response): Promise<Response> => {
     const orders = await this.service.getAll();
     return res.status(StatusCodes.OK).json(orders);
+  };
+
+  public create = async (req: Request, res: Response, next: NextFunction):
+  Promise<Response | void> => {
+    try {
+      const { productsIds } = req.body;
+      const userId = req.body.user;
+
+      const newOrder = await this.service.create(userId, productsIds);
+
+      return res.status(StatusCodes.CREATED).json(newOrder);
+    } catch (error) {
+      next(error);
+    }
   };
 }
 
