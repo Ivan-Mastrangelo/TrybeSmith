f2723e2b970704fff2040956980f7b20e50cade3
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-05-17 20:33:53 -0300

    feat: implementar função, na classe OrderService, que cria nova order na tabela Orders, recupera seu id e atualiza a tabela Product

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,12 +1,26 @@
 import OrderModel from '../models/orders.model';
+import ProductModel from '../models/products.model';
 import IOrder from '../interfaces/order.interface';
 
 class OrderService {
   public model = new OrderModel();
 
+  public prodModel = new ProductModel();
+
   public getAll = async (): Promise<IOrder[]> => {
     const orders = await this.model.getAll();
     return orders;
+  };
+
+  public create = async (userId: number, productsIds: number[]): Promise<IOrder> => {
+    const orderId = await this.model.create(userId);
+    Promise.all(productsIds.map(async (id: number): Promise<void> => {
+      await this.prodModel.upDate(orderId, id);
+    }));
+    return {
+      userId,
+      productsIds,
+    } as IOrder;
   };
 }
 
